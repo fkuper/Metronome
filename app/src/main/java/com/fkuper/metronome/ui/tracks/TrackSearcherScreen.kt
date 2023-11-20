6414e7dbba28b5362f18e34495ae44269a471793
Frederik Kuper <frederikkuper@gmail.com>
2023-11-20 11:54:11 +0100

    add search text field to search screen

--- app/src/main/java/com/fkuper/metronome/ui/tracks/TrackSearcherScreen.kt
+++ app/src/main/java/com/fkuper/metronome/ui/tracks/TrackSearcherScreen.kt
@@ -1,22 +1,25 @@
 package com.fkuper.metronome.ui.tracks
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Remove
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,9 +30,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.fkuper.metronome.R
@@ -75,7 +82,12 @@ private fun TrackSearcherScreenBody(
         verticalArrangement = Arrangement.Top,
         modifier = modifier
     ) {
-        TrackSearchBar(onSearchForTracks = { onSearchForTracks(it) })
+        TrackSearchBar(
+            onSearchForTracks = { onSearchForTracks(it) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+        )
         
         if (tracks == null) return
         LazyColumn {
@@ -124,18 +136,35 @@ private fun SpotifyTrackRow(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun TrackSearchBar(
-    onSearchForTracks: (String) -> Unit
+    onSearchForTracks: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    // TODO: implement text field and use its value as input
-    val searchString = "test"
+    var searchString by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
 
-    Row {
-        Button(onClick = { onSearchForTracks(searchString) }) {
-            Text("Search for tracks")
-        }
-    }
+    OutlinedTextField(
+        value = searchString,
+        onValueChange = { searchString = it },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = stringResource(id = R.string.search_prompt_icon)
+            )
+        },
+        label = {
+            Text(stringResource(id = R.string.search_prompt))
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = {
+            onSearchForTracks(searchString)
+            focusManager.clearFocus()
+        }),
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -145,12 +174,8 @@ private fun AlbumArtView(url: String) {
         contentDescription = null,
         modifier = Modifier
             .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
-            .clip(RoundedCornerShape(10))
-            .border(
-                border = BorderStroke(
-                    width = dimensionResource(R.dimen.border_width_large),
-                    color = MaterialTheme.colorScheme.surfaceTint
-                ),
+            .shadow(
+                elevation = 5.dp,
                 shape = RoundedCornerShape(10)
             )
     )
