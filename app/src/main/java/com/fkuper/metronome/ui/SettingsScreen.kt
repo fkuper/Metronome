79c55a06d8dd0c146e4f6b61fb6de7b44a532a8f
Frederik Kuper <frederikkuper@gmail.com>
2023-12-12 15:59:30 +0100

    add info dialog to explain practice reminders

--- app/src/main/java/com/fkuper/metronome/ui/SettingsScreen.kt
+++ app/src/main/java/com/fkuper/metronome/ui/SettingsScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -18,6 +20,7 @@ import androidx.compose.material.icons.rounded.DisplaySettings
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.material.icons.rounded.QuestionMark
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -28,13 +31,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chargemap.compose.numberpicker.FullHours
 import com.chargemap.compose.numberpicker.Hours
@@ -88,6 +96,14 @@ private fun SettingsScreenBody(
     onDisplayThemeChanged: (DisplayTheme) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var showInfoDialog by remember { mutableStateOf(false) }
+
+    if (showInfoDialog) {
+        PracticeRemindersInfoDialog(onDismissRequest = {
+            showInfoDialog = false
+        })
+    }
+
     Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         SectionHeading(icon = Icons.Rounded.Notifications, title = "Notifications")
         Row(
@@ -114,9 +130,7 @@ private fun SettingsScreenBody(
                     }
                 } else null
             )
-            IconButton(onClick = {
-                // TODO: implement info popup to tell user about practice reminder notifications
-            }) {
+            IconButton(onClick = { showInfoDialog = true }) {
                 Icon(imageVector = Icons.Rounded.QuestionMark, contentDescription = null)
             }
         }
@@ -137,6 +151,27 @@ private fun SettingsScreenBody(
         )
         SectionHeading(icon = Icons.Rounded.Info, title = "Info")
         // TODO: implement info section (attributions, info about me etc.)
+    }
+}
+
+@Composable
+private fun PracticeRemindersInfoDialog(onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            Text(
+                text = stringResource(id = R.string.practice_reminder_info_text),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center)
+                    .padding(dimensionResource(id = R.dimen.padding_small)),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
